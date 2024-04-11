0334d7d134a8aec941bcd971d760eb83b11a9f9f
DESKTOP-KGFU7DG\david <davidrizescu@yahoo.com>
2024-04-11 19:21:58 +0300

    concatenation examples

---
+++ CursStrings/CursStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+        // Declare strings
+        string myFirstString = "Hello, World!";
+        Console.WriteLine(myFirstString);
+
+        int myNumber = 42;
+        Console.WriteLine(myNumber);    
+
+        string myNumberAsString = myNumber.ToString();
+
+        // Manipulating strings;
+        // Concatenation;
+        string firstName = "Ionel";
+        string lastName = "Popescu";
+        string fullName = firstName + " " + lastName;
+
+
+        string fullNameWithConcat = String.Concat(firstName, " ", lastName);
+        Console.WriteLine(fullName);
+        Console.WriteLine(fullNameWithConcat);
+        Console.WriteLine($"{firstName} {lastName}");
+
+    }
+}
